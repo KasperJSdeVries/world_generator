b59810341f996765eef11e9b7eb32612c1902573
Kasper <89792927+KasperJSdeVries@users.noreply.github.com>
2023-09-18 00:49:37 +0200

    Create vulkan debug messenger

--- src/renderer/vulkan/vulkan_backend.c
+++ src/renderer/vulkan/vulkan_backend.c
@@ -7,15 +7,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-b8 create_instance(vulkan_context *context, const char *application_name);
+b8 create_instance(const char *application_name, VkInstance *out_instance);
+#if defined(_DEBUG)
+void create_debug_messenger(VkInstance instance,
+							VkDebugUtilsMessengerEXT *out_debug_messenger);
+#endif
 
 b8 backend_initialize(void **out_renderer_context) {
 	*out_renderer_context = malloc(sizeof(vulkan_context));
 	vulkan_context *context = *out_renderer_context;
 
-	if (!create_instance(context, "World Generator")) {
+	if (!create_instance("World Generator", &context->instance)) {
 		return false;
 	}
+
+#if defined(_DEBUG)
+	create_debug_messenger(context->instance, &context->debug_messenger);
+#endif
 
 	return true;
 }
@@ -23,11 +31,18 @@ b8 backend_initialize(void **out_renderer_context) {
 void backend_shutdown(void *renderer_context) {
 	vulkan_context *context = renderer_context;
 
+#if defined(_DEBUG)
+	PFN_vkDestroyDebugUtilsMessengerEXT destroy_debug_messenger_function =
+		(PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+			context->instance, "vkDestroyDebugUtilsMessengerEXT");
+	destroy_debug_messenger_function(context->instance,
+									 context->debug_messenger, NULL);
+#endif
 	vkDestroyInstance(context->instance, NULL);
 	free(context);
 }
 
-b8 create_instance(vulkan_context *context, const char *application_name) {
+b8 create_instance(const char *application_name, VkInstance *out_instance) {
 	VkApplicationInfo application_info = {};
 	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	application_info.apiVersion = VK_API_VERSION_1_3;
@@ -93,9 +108,47 @@ b8 create_instance(vulkan_context *context, const char *application_name) {
 	create_info.enabledLayerCount = required_validation_layer_count;
 	create_info.ppEnabledLayerNames = required_validation_layer_names;
 
-	if (vkCreateInstance(&create_info, NULL, &context->instance) !=
-		VK_SUCCESS) {
+	if (vkCreateInstance(&create_info, NULL, out_instance) != VK_SUCCESS) {
 		return false;
 	}
 	return true;
 }
+
+#if defined(_DEBUG)
+VKAPI_ATTR VkBool32 VKAPI_CALL
+vk_debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+				  VkDebugUtilsMessageTypeFlagBitsEXT message_types,
+				  const VkDebugUtilsMessengerCallbackDataEXT *callback_data,
+				  void *user_data) {
+	switch (message_severity) {
+	default:
+		printf("%s\n", callback_data->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+		fprintf(stderr, "%s\n", callback_data->pMessage);
+		break;
+	}
+	return VK_FALSE;
+}
+
+void create_debug_messenger(VkInstance instance,
+							VkDebugUtilsMessengerEXT *out_debug_messenger) {
+	u32 log_severity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+					   VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
+	VkDebugUtilsMessengerCreateInfoEXT create_info = {};
+	create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	create_info.messageSeverity = log_severity;
+	create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+							  VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+							  VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	create_info.pfnUserCallback = vk_debug_callback;
+	create_info.pUserData = 0;
+
+	PFN_vkCreateDebugUtilsMessengerEXT create_function =
+		(PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+			instance, "vkCreateDebugUtilsMessengerEXT");
+	if (create_function(instance, &create_info, NULL, out_debug_messenger)!= VK_SUCCESS) {
+		fprintf(stderr, "Failed to create Debug Messenger!\n");
+	}
+}
+#endif
